9ce61d70f2c23c7f843f1ecefa1e72374baba15a
Parikshit Khanna <52244689+parikshitkhanna-prog@users.noreply.github.com>
2024-01-16 10:17:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+function redirectToNewPage() {
+  window.open("rose-activity.html", "_blank");
+}
+function redirectToOhYeahPage() {
+  window.open("oh-yeahh.html", "_blank");
+}
+function redirectToPromActPage() {
+  window.open("prom-activity.html", "_blank");
+}
+function redirectToKissActPage() {
+  window.open("kiss-activity.html", "_blank");
+}
+function redirectToValentinePage() {
+  window.open("valentine.html", "_blank");
+}
